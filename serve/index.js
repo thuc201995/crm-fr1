b73a76d27511c374c191071d43ecb75aff71129c
thuc201995 <thuc201995@gmail.com>
2021-03-25 10:07:53 +0700

    temp

--- serve/index.js
+++ serve/index.js
@@ -1,11 +1,32 @@
+// const handler = require("serve-handler");
+const http = require('http');
 const express = require('express');
-const path = require('path');
 const app = express();
-
+const httpApp = express();
+const spdy = require('spdy');
+const path = require('path');
+const fs = require('fs');
+const compression = require('compression');
+var options = {
+  key: fs.readFileSync('/etc/ssl/private/xoso.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/xoso.crt'),
+  ca: fs.readFileSync('/etc/ssl/certs/xoso_ca_bundle.crt'),
+};
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(1111);
+const server = spdy.createServer(options, app);
+const serverHttp = http.createServer(httpApp);
+httpApp.get('*', function (req, res) {
+  res.redirect('https://' + req.headers.host + req.url);
+});
+
+// // have it listen on 80
+serverHttp.listen(1112);
+server.listen(1111, () => {
+  console.log('Running at http://localhost:3000');
+});
