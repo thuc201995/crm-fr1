3652660bb7f126377794529c62a2a5778f18df7a
thuc201995 <thuc201995@gmail.com>
2021-03-25 08:30:49 +0700

    chang config serve

--- serve/index.js
+++ serve/index.js
@@ -1,46 +1,11 @@
-// const handler = require("serve-handler");
-const http = require('http');
 const express = require('express');
-const app = express();
-const httpApp = express();
-const spdy = require('spdy');
 const path = require('path');
-const fs = require('fs');
-const compression = require('compression');
-var options = {
-  key: fs.readFileSync('/etc/ssl/private/xoso.key'),
-  cert: fs.readFileSync('/etc/ssl/certs/xoso.crt'),
-  ca: fs.readFileSync('/etc/ssl/certs/xoso_ca_bundle.crt'),
-};
-app.use(compression());
+const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/sitemap', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'sitemaps', 'sitemap.xml'));
-});
-app.get('/sitemap1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'sitemaps', 'sitemap.xml'));
-});
-app.get('/sitemapMB', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'sitemaps', 'sitemapMB.xml'));
-});
-app.get('/sitemapMN', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'sitemaps', 'sitemapMN.xml'));
-});
-app.get('/sitemapMT', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'sitemaps', 'sitemapMT.xml'));
-});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const server = spdy.createServer(options, app);
-const serverHttp = http.createServer(httpApp);
-httpApp.get('*', function (req, res) {
-  res.redirect('https://' + req.headers.host + req.url);
-});
-
-// // have it listen on 80
-serverHttp.listen(1111);
-server.listen(443, () => {
-  console.log('Running at http://localhost:3000');
-});
+app.listen(1111);
